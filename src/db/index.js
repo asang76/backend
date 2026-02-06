8b0d4ad8a514f1f1d9237c9a34f32f879aaa498b
asang76 <asangtamgadge70@gmail.com>
2026-02-06 21:08:31 +0530

    added packages

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async () => {
+    try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log(`\n MongoDB connected successfully to host: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("connection failed from ConnectDB",error);
+        process.exit(1);
+    }
+}
+console.log(process.env.MONGODB_URI,"MONGODB_URIMONGODB_URIMONGODB_URI")
+export default connectDB;
