b4a8be42010d3e84b4030ab6dbab5ebb8ca4f8f0
asang76 <asangtamgadge70@gmail.com>
2026-02-07 21:00:57 +0530

    utils code added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-// require("dotenv").config({path:"./.env"});
-import dotenv from "dotenv";
-import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+dotenv.config({path:"./.env"})
+import connectDB from "./db/index.js"
 
-
-
-dotenv.config({path:"./.env"});
-connectDB();
-
-
-
-
-
-
-
-
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`server eunning at port ${process.env.PORT}`)
+    })
+})
+.catch((err) =>{
+    console.log("mongo db Connection FDailes")
+})
